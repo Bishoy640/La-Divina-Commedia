22b32b9bf6581d19115ad130b0275e9fbdd206b3
qwasd <dasdsa>
2021-06-13 17:18:22 +0200

    شوف_شغلك_بقى

---
+++ Dante_Relase/js/commedia.js
@@ -0,0 +1,161 @@
+var input = document.getElementById("myInput");
+var value1 = document.getElementById("myInput").value;
+var answer = "";
+var answerlen = 0;
+var found = false;
+let end = "l'amor che move il sole e l'altre stelle.";
+var answercanto = "";
+
+$("#myInput").keyup(function (event) {
+  if (event.keyCode === 13) {
+    $(".btn-search").click();
+  }
+});
+
+function search() {
+  console.log("worked");
+  console.log(document.getElementById("myInput").value);
+
+  $.getJSON("divina_commedia.json", function (data) {
+    answer = "";
+    for (let cantica of data["children"]) {
+      for (let canto of cantica["children"]) {
+        for (let terzine of canto["children"]) {
+          for (let riga of terzine["children"]) {
+            riga["text"] = riga["text"]
+              .replace("’", "'")
+              .replace("‘", "'")
+              .replace("”", '"')
+              .replace("\u2019", "'")
+              .replace("\u00ec", "i");
+            if (
+              riga["text"].includes(document.getElementById("myInput").value)
+            ) {
+              answer +=
+                "Cantica: " +
+                cantica["name"] +
+                "<br>" +
+                "Canto: " +
+                canto["name"] +
+                "<br>" +
+                "Terzina: " +
+                terzine["number"] +
+                "<br>" +
+                "Riga: " +
+                riga["number"];
+
+              answer += "<br><br><hr><br><br>";
+            }
+
+            // console.log(riga['text'])
+            // if (MyArray[0] == "che 'l tradimento insieme con lui fece."){ s7
+            //   console.log("ez")
+            // }
+            // if (riga['text'] == MyArray[0]) {
+            //     alert("Cantica: " + cantica['name'] + "\n" + "Canto: " + canto['name'] + "\n" + "Riga: " + riga['number'])
+            //     console.log("Cantica:", cantica['name'])
+            //     console.log("Canto:", canto['name'])
+            //     console.log("Riga:", riga['number'])
+            // }
+          }
+        }
+        console.log(canto["name"]);
+      }
+    }
+    answerlen = answer.length;
+    if (answer.endsWith("<br><br><hr><br><br>")) {
+      answer = answer.substring(0, answerlen - 21);
+      console.log("iam here");
+    }
+    if (answerlen == 0) {
+      answer = "Non rovato";
+    }
+    document.getElementById("result").innerHTML = answer;
+    $(document).ready(function () {
+      $("#modalSearch").modal();
+    });
+
+    console.log("finished");
+  });
+}
+
+function getSelectValue(list) {
+  if (list == "list1") {
+    let selectElement1 = document.getElementById("list1");
+    let valueSelected1 =
+      selectElement1.options[selectElement1.selectedIndex].value; // get selected option value
+    var text1 = selectElement1.options[selectElement1.selectedIndex].text; //get the selected option text
+
+    if (valueSelected1.length != 0 && valueSelected1.length != 16) {
+      resultFin("Inferno", text1);
+      document.getElementById("Presult2").innerHTML = answer;
+      $(document).ready(function () {
+        $("#modalCanto").modal();
+      });
+    }
+  }
+
+  if (list == "list2") {
+    let selectElement2 = document.getElementById("list2");
+    let valueSelected2 =
+      selectElement2.options[selectElement2.selectedIndex].value; // get selected option value
+    var text2 = selectElement2.options[selectElement2.selectedIndex].text; //get the selected option text
+    if (text2.length != 0 && valueSelected2.length != 16) {
+      resultFin("Purgatorio", text2);
+      document.getElementById("Presult2").innerHTML = answer;
+      $(document).ready(function () {
+        $("#modalCanto").modal();
+      });
+    }
+  }
+
+  if ((list = "list3")) {
+    let selectElement3 = document.getElementById("list3");
+    let valueSelected3 =
+      selectElement3.options[selectElement3.selectedIndex].value; // get selected option value
+    var text3 = selectElement3.options[selectElement3.selectedIndex].text; //get the selected option text
+
+    if (valueSelected3.length != 0 && valueSelected3.length != 16) {
+      resultFin("Paradiso", text3);
+      document.getElementById("Presult2").innerHTML = answer;
+      $(document).ready(function () {
+        $("#modalCanto").modal();
+      });
+    }
+  }
+}
+
+function resultFin(CantoName, CantoNum) {
+  $.getJSON("divina_commedia.json", function (data) {
+    for (let cantica of data["children"]) {
+      if (cantica["name"] == CantoName) {
+        document.getElementById("CantoName").innerHTML = CantoName;
+        for (let canto of cantica["children"]) {
+          if (canto["name"] == CantoNum) {
+            document.getElementById("result2").innerHTML = CantoNum;
+            for (let terzine of canto["children"]) {
+              if (answer.length != 0) {
+                answer += "<br>";
+              }
+              for (let riga of terzine["children"]) {
+                riga["text"] = riga["text"]
+                  .replace("’", "'")
+                  .replace("‘", "'")
+                  .replace("”", '"')
+                  .replace("\u2019", "'")
+                  .replace("\u00ec", "i");
+
+                answer += riga["text"];
+                answer += "<br>";
+                document.getElementById("Presult2").innerHTML = answer;
+                $(document).ready(function () {
+                  $("#modalCanto").modal();
+                });
+              }
+            }
+          }
+        }
+      }
+    }
+  });
+}
---
+++ Dante_Relase/js/upload.js
@@ -0,0 +1,142 @@
+function readURLL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      $(".image-upload-wrap").hide();
+
+      $(".file-upload-image").attr("src", e.target.result);
+      result = e.target.result;
+      $(".file-upload-content").show();
+
+      $(".image-title").html(input.files[0].name);
+    };
+
+    reader.readAsDataURL(input.files[0]);
+  } else {
+    removeUpload();
+  }
+  if (reader.readyState == 2) {
+    alert("a");
+  }
+}
+
+let MyArray = [];
+var answer = "";
+var found = false;
+let end = "l'amor che move il sole e l'altre stelle.";
+var terzinaRes = "";
+function tess() {
+  Tesseract.recognize(result, "ita", { logger: (m) => console.log("m") })
+    .then(({ data: { text } }) => {
+      MyArray = text.split("\n");
+      MyArray[0] = MyArray[0]
+        .replace("’", "'")
+        .replace("‘", "'")
+        .replace("”", '"');
+
+      $.getJSON("divina_commedia.json", function (data) {
+        for (let cantica of data["children"]) {
+          for (let canto of cantica["children"]) {
+            for (let terzine of canto["children"]) {
+              for (let riga of terzine["children"]) {
+                riga["text"] = riga["text"]
+                  .replace("’", "'")
+                  .replace("‘", "'")
+                  .replace("”", '"')
+                  .replace("\u2019", "'")
+                  .replace("\u00ec", "i");
+
+                if (riga["text"] == MyArray[0]) {
+                  getTerzina(cantica["name"], canto["name"], terzine["number"])
+                  answer =
+                    "Cantica: " +
+                    cantica["name"] +
+                    "<br>" +
+                    "Canto: " +
+                    canto["name"] +
+                    "<br>" +
+                    "Terzina: " +
+                    terzine["number"] +
+                    "<br>" +
+                    "Riga: " +
+                    riga["number"];
+                  
+                  document.getElementById("result").innerHTML = answer;
+
+                  $(document).ready(function () {
+                    $("#modalresult").modal();
+                    found = true;
+                  });
+                  // console.log("aas")
+                  // document.getElementById("result").innerHTML="Errore Successo";
+                  // $(document).ready(function(){
+                  //   $("#modalresult").modal();
+                  // });
+                } else if (!found && riga["text"] == end) {
+                  document.getElementById("result").innerHTML = "Non Trovato";
+                  $(document).ready(function () {
+                    $("#modalresult").modal();
+                  });
+                }
+              }
+            }
+          }
+        }
+        console.log(MyArray[0]);
+        console.log("finished");
+      });
+    })
+    .then(function hide_loader() {
+      $(".loader1").addClass("hide-loader");
+      $(".hide-loader").removeClass("loader1");
+    });
+}
+
+function submit() {
+  $(".hide-loader").addClass("loader1");
+  $(".loader1").removeClass("hide-loader");
+
+  tess();
+}
+
+function removeUpload() {
+  $(".file-upload-input").replaceWith($(".file-upload-input").clone());
+  $(".file-upload-content").hide();
+  $(".image-upload-wrap").show();
+}
+$(".image-upload-wrap").bind("dragover", function () {
+  $(".image-upload-wrap").addClass("image-dropping");
+});
+$(".image-upload-wrap").bind("dragleave", function () {
+  $(".image-upload-wrap").removeClass("image-dropping");
+});
+
+function getTerzina(cantoName, cantoNum, TrezinaNum) {
+  $.getJSON("divina_commedia.json", function (data) {
+    for (let cantica of data["children"]) {
+      if (cantica["name"] == cantoName) {
+        for (let canto of cantica["children"]) {
+          if (canto["name"] == cantoNum) {
+            for (let terzine of canto["children"]) {
+              if (terzine["number"] == TrezinaNum) {
+                for (let riga of terzine["children"]) {
+                  riga["text"] = riga["text"]
+                    .replace("’", "'")
+                    .replace("‘", "'")
+                    .replace("”", '"')
+                    .replace("\u2019", "'")
+                    .replace("\u00ec", "i");
+                  terzinaRes += riga["text"] + "<br>";
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    document.getElementById("resultMark").innerHTML = terzinaRes;
+  });
+  
+
+}
